67fb280c26290e37513a46c12081f79165c7e014
murali <muralimano86@gmail.com>
2024-02-06 19:09:08 +0000

    Adding titles from csv file to web page in 2 columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -17,3 +18,15 @@ content2 = """
 Below are some of the apps I built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.title(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.title(row["title"])
